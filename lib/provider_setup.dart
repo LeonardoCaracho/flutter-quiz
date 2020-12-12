14e2e9afeabfefab56552ff87abe535c0fa82368
Leonardo <leeo.apc@gmail.com>
2020-12-12 16:45:48 -0300

    remove unnecessary service

--- lib/provider_setup.dart
+++ lib/provider_setup.dart
@@ -2,7 +2,6 @@ import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 import 'core/services/api.dart';
-import 'core/services/quiz_service.dart';
 
 List<SingleChildWidget> providers = [
   ...independentServices,
@@ -14,10 +13,6 @@ List<SingleChildWidget> independentServices = [
   Provider.value(value: Api()),
 ];
 
-List<SingleChildWidget> dependentServices = [
-  ProxyProvider<Api, QuizService>(
-    update: (_, api, __) => QuizService(api: api),
-  ),
-];
+List<SingleChildWidget> dependentServices = [];
 
 List<SingleChildWidget> uiConsumableProviders = [];
